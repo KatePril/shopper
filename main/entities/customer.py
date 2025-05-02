d079a9bace3401100cec4b9406a9ea3cae8ea987
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-02 21:28:22 +0300

    update customer

--- main/entities/customer.py
+++ main/entities/customer.py
@@ -1,12 +1,12 @@
 class Customer:
     def __init__(self, first_name, last_name, email, phone_number, password, customer_id=None):
         if customer_id is not None:
-            self.customer_id = customer_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.phone_number = phone_number
-        self.password = password
+            self.__customer_id = customer_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__email = email
+        self.__phone_number = phone_number
+        self.__password = password
 
     @classmethod
     def from_dict(cls, customer_dict):
@@ -21,26 +21,36 @@ class Customer:
 
     def to_dict(self):
         customer = {
-                'first_name': self.first_name,
-                'last_name': self.last_name,
-                'email': self.email,
-                'phone_number': self.phone_number,
-                'password': self.password,
-                'salt': self.salt
+                'first_name': self.__first_name,
+                'last_name': self.__last_name,
+                'email': self.__email,
+                'phone_number': self.__phone_number,
+                'password': self.__password
             }
-        if self.customer_id is not None:
-            customer['customer_id'] = self.customer_id
+        if self.__customer_id is not None:
+            customer['customer_id'] = self.__customer_id
         return customer
 
+    @property
+    def email(self):
+        return self.__email
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, new_password):
+        self.__password = new_password
 
     def __eq__(self, other):
         if not isinstance(other, Customer):
             return NotImplemented
         return (
-            self.customer_id == other.customer_id and
-            self.first_name == other.first_name and
-            self.last_name == other.last_name and
-            self.email == other.email and
-            self.phone_number == other.phone_number and
-            self.password == other.password
+            self.__customer_id == other.customer_id and
+            self.__first_name == other.first_name and
+            self.__last_name == other.last_name and
+            self.__email == other.email and
+            self.__phone_number == other.phone_number and
+            self.__password == other.password
         )
