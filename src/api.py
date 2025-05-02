b1039f12a9667b2a2bfde9629501529d0f877f7f
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-02 23:12:02 +0300

    Refactor imports in api

--- src/api.py
+++ src/api.py
@@ -2,16 +2,11 @@ from flask import Flask, render_template, request, session
 from dotenv import load_dotenv
 import os
 
-#from src.entities.customer import Customer
-#from src.registration_login.create_customer import create_customer
-#from src.registration_login.password import generate_password_hash
-#from src.registration_login.check_login import check_login
-#from src.database_connection import cursor, conn
 from src.registration_login.landing import process_landing
 app = Flask(__name__)
 
 load_dotenv()
-# app.secret_key = os.getenv("APP_SECRET_KEY")
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
